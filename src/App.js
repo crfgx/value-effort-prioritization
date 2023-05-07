b450f2f8d4b9a5eeb6784f82b9b9336ea2f5b92b
Cagdas A <cagirtas@gmail.com>
2023-05-07 11:32:14 +0300

    v0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,238 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Scatter } from "react-chartjs-2";
+import "chart.js/auto";
+import background from "./image.svg";
 
-function App() {
+const App = () => {
+  const [value1, setValue1] = useState("low");
+  const [effort1, setEffort1] = useState("sm");
+  const [feature1, setFeature1] = useState("feature1");
+  const [value2, setValue2] = useState("high");
+  const [effort2, setEffort2] = useState("large-xl");
+  const [feature2, setFeature2] = useState("feature2");
+
+  const data = {
+    datasets: [
+      {
+        label: "Value / Effort Chart",
+        data: [
+          { x: 1, y: 1, label: "sm_low" },
+          {
+            x: 1,
+            y: 2,
+            label: "sm_high",
+          },
+          { x: 2, y: 1, label: "large-xl_low" },
+          { x: 2, y: 2, label: "large-xl_high" },
+        ],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: "linear",
+        min: 0,
+        max: 3,
+        title: {
+          display: true,
+          text: "Effort",
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return ["", "Small-Medium", "Large-XL"][value];
+          },
+        },
+      },
+      y: {
+        type: "linear",
+        min: 0,
+        max: 3,
+        title: {
+          display: true,
+          text: "Value",
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return ["", "Low", "High"][value];
+          },
+        },
+      },
+    },
+  };
+
+  const handleChangeValue1 = (event) => {
+    setValue1(event.target.value);
+  };
+
+  const handleChangeEffort1 = (event) => {
+    setEffort1(event.target.value);
+  };
+
+  const handleChangeFeature1 = (event) => {
+    setFeature1(event.target.value);
+  };
+
+  const handleChangeValue2 = (event) => {
+    setValue2(event.target.value);
+  };
+
+  const handleChangeEffort2 = (event) => {
+    setEffort2(event.target.value);
+  };
+
+  const handleChangeFeature2 = (event) => {
+    setFeature2(event.target.value);
+  };
+
+  const feature1Data = data.datasets[0].data.filter((item) =>
+    item.label.includes(`${effort1}_${value1}`)
+  );
+
+  const feature2Data = data.datasets[0].data.filter((item) =>
+    item.label.includes(`${effort2}_${value2}`)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div
+      className="all"
+      style={{
+        display: "flex",
+        justifycontent: "flex-end",
+        margin: 10,
+      }}
+    >
+      <div
+        className="feature_list"
+        style={{
+          display: "inline-block",
+        }}
+      >
+        <div className="feature1" style={{ marginRight: "20px" }}>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="feature">Feature 1</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              id="text"
+              value={feature1}
+              onChange={handleChangeFeature1}
+            />
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="value1">Value</label>
+            <div>
+              <select id="value" value={value1} onChange={handleChangeValue1}>
+                <option value="low">Low</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="effort">Effort</label>
+            <div>
+              <select
+                id="effort"
+                value={effort1}
+                onChange={handleChangeEffort1}
+              >
+                <option value="sm">Small-Medium</option>
+                <option value="large-xl">Large-XL</option>
+              </select>
+            </div>
+          </div>
+          <hr className="dashed" style={{ marginTop: 10 }}></hr>
+        </div>
+        <div className="feature2" style={{ marginRight: "20px" }}>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="feature2">Feature 2</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              id="text"
+              value={feature2}
+              onChange={handleChangeFeature2}
+            />
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="value">Value</label>
+            <div>
+              <select id="value" value={value2} onChange={handleChangeValue2}>
+                <option value="low">Low</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <label htmlFor="effort">Effort</label>
+            <div>
+              <select
+                id="effort"
+                value={effort2}
+                onChange={handleChangeEffort2}
+              >
+                <option value="sm">Small-Medium</option>
+                <option value="large-xl">Large-XL</option>
+              </select>
+            </div>
+          </div>
+          <hr className="dashed" style={{ marginTop: 10 }}></hr>
+        </div>
+      </div>
+      <div
+        className="chart"
+        style={{
+          width: "100%",
+          marginTop: 20,
+          backgroundImage: `url(${background})`,
+          backgroundPosition: "center",
+          backgroundSize: "70%",
+          backgroundRepeat: "no-repeat",
+          opacity: 0.7,
+        }}
+      >
+        <Scatter
+          data={{
+            datasets: [
+              {
+                label: feature1,
+                data: feature1Data,
+                pointRadius: 40,
+                pointBackgroundColor: "red",
+                pointBorderWidth: 2,
+                pointBorderColor: "black",
+              },
+              {
+                label: feature2,
+                data: feature2Data,
+                pointRadius: 40,
+                pointBackgroundColor: "blue",
+                pointBorderWidth: 2,
+                pointBorderColor: "black",
+              },
+            ],
+          }}
+          options={options}
+        />
+      </div>
+      <div
+        className="footer"
+        style={{ position: "absolute", bottom: "3px", right: "1%" }}
+      >
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          style={{ fontSize: 14, color: "grey" }}
+          href="https://www.linkedin.com/in/cagirtas/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          cagirtas 🫶
         </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
